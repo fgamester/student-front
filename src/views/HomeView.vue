fc6056cfac093939c5bf8d46b8e81ffaf99c7479
fgamester <falvarez.munoz@hotmail.es>
2025-05-13 14:07:34 -0400

    H^{3}HO

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,33 +1,14 @@
-<script lang="ts" setup>
-// font-family: Arial, sans-serif;
-const example =
-  "<span style=\"font-family: 'Times New Roman', serif; font-size: 16px; padding-bottom: 1em; display: inline-block; vertical-align: middle; position: relative;\">\n" +
-  "  max\n" +
-  '  <span style="position: absolute; font-size: 0.7em; bottom: 0.6em; left: 0; right: 0; text-align: start; line-height: 1;">\n' +
-  "    0≤x≤1\n" +
-  "  </span>\n" +
-  '  <span style="margin-left: 0.2em;">\n' +
-  '    xe<span style="vertical-align: super; font-size: smaller;">-x<span style="font-size: smaller;">2</span></span>\n' +
-  "  </span>\n" +
-  "</span>";
-console.log(example.replace("\\", ""));
-</script>
-
+<script lang="ts" setup></script>
 <template>
   <span
     style="
-      font-size: 16px;
       font-family: 'Times New Roman', Times, serif;
-      display: inline;
+      font-size: 16px;
+      font-style: italic;
     "
   >
-    <span style="display: inline-flex; flex-direction: column; font-size: 0.7em;">
-      <span style="line-height: 0.88;">227</span>
-      <span style="line-height: 0.88;">90</span>
-    </span>
-    <span style="font-style: italic; vertical-align: -7px;">
-      <span >Th</span>
-      <span style="vertical-align: super; font-size: 0.7em;">+</span>
-    </span>
+    <span style="margin-right: 0.1em">H</span>
+    <span style="vertical-align: super">3</span>
+    <span>HO</span>
   </span>
 </template>
