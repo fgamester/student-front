0cf4062e83316307940539f4498459cceca7081b
fgamester <falvarez.munoz@hotmail.es>
2025-05-13 16:58:50 -0400

    \\tfrac{1}{2} H_{2}O

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,6 @@
 <script lang="ts" setup>
-export const inString = "<span style=\"font-family: 'Times New Roman', Times, serif; font-size: 16px; font-style: italic;\"><span style=\"margin-right: 0.1em\">H</span><span style=\"vertical-align: super\">3</span><span>HO</span></span>";
+const inString ="<span style=\"font-family: 'Times New Roman', Times, serif; font-size: 16px; font-style: italic;\"><span style=\"display: inline-flex; flex-direction: column; line-height: 0.9; font-size: 0.8em; vertical-align: middle; margin-right: 0.2em;\"><span>1</span><span style=\"margin-top: -1em; margin-bottom: 0.1em; font-weight: 900;\">_</span><span>2</span></span><span><span>H</span><span style=\"font-size: 0.7em; vertical-align: sub;\">2</span><span>O</span></span></span>";
+console.log(inString);
 </script>
 <template>
   <span
@@ -9,8 +10,26 @@ export const inString = "<span style=\"font-family: 'Times New Roman', Times, se
       font-style: italic;
     "
   >
-    <span style="margin-right: 0.1em">H</span>
-    <span style="vertical-align: super">3</span>
-    <span>HO</span>
+    <span
+      style="
+        display: inline-flex;
+        flex-direction: column;
+        line-height: 0.9;
+        font-size: 0.8em;
+        vertical-align: middle;
+        margin-right: 0.2em;
+      "
+    >
+      <span>1</span>
+      <span style="margin-top: -1em; margin-bottom: 0.1em; font-weight: 900"
+        >_</span
+      >
+      <span>2</span>
+    </span>
+    <span>
+      <span>H</span>
+      <span style="font-size: 0.7em; vertical-align: sub">2</span>
+      <span>O</span>
+    </span>
   </span>
 </template>
