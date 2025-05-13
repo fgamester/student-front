193c3e9852429923e4d5a7bc518b1e57604d6d13
fgamester <falvarez.munoz@hotmail.es>
2025-05-13 16:23:31 -0400

    H^{3}HO

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,6 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+export const inString = "<span style=\"font-family: 'Times New Roman', Times, serif; font-size: 16px; font-style: italic;\"><span style=\"margin-right: 0.1em\">H</span><span style=\"vertical-align: super\">3</span><span>HO</span></span>";
+</script>
 <template>
   <span
     style="
