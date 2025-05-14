c44a0b987969ce059eabd0134f9ee420b476bd30
fgamester <falvarez.munoz@hotmail.es>
2025-05-13 23:47:33 -0400

    CrO_{4}^{2-}

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,16 @@
 <template>
   <span style="font-family: 'Times New Roman', Times, serif; display: inline-block; font-size: 16px;">
-    <span style="font-style: italic;">nH</span>
-    <span style="vertical-align: sub; font-size: 0.75em;">2</span>
-    <span style="font-style: italic;">O</span>
+    <span style="font-style: italic;">CrO</span>
+    <span style="display: inline-flex; flex-direction: column; line-height: 0.75em; vertical-align: middle; margin-left: 0.05em;">
+      <span style="font-size: 0.75em;">2–</span>
+      <span style="font-size: 0.75em;">4</span>
+    </span>
   </span>
 </template>
 <script lang="ts" setup>
 const completed = {
   "": "",
+  "CrO_{4}^{2-}": "<span style=\"font-family: 'Times New Roman', Times, serif; display: inline-block; font-size: 16px;\"><span style=\"font-style: italic;\">CrO</span><span style=\"display: inline-flex; flex-direction: column; line-height: 0.75em; vertical-align: middle; margin-left: 0.05em;\"><span style=\"font-size: 0.75em;\">2–</span><span style=\"font-size: 0.75em;\">4</span></span></span>",
   "n H_{2}O": "<span style=\"font-family: 'Times New Roman', Times, serif; display: inline-block; font-size: 16px;\"><span style=\"font-style: italic;\">nH</span><span style=\"vertical-align: sub; font-size: 0.75em;\">2</span><span style=\"font-style: italic;\">O</span></span>",
   "\\tfrac{1}{2} H_{2}O": "<span style=\"font-family: 'Times New Roman', Times, serif; font-size: 16px; font-style: italic;\"><span style=\"display: inline-flex; flex-direction: column; line-height: 0.9; font-size: 0.8em; vertical-align: middle; margin-right: 0.2em;\"><span>1</span><span style=\"margin-top: -1em; margin-bottom: 0.1em; font-weight: 900;\">_</span><span>2</span></span><span><span>H</span><span style=\"font-size: 0.7em; vertical-align: sub;\">2</span><span>O</span></span></span>",
   "H^{3}HO": "<span style=\"font-family: 'Times New Roman', Times, serif; font-size: 16px; font-style: italic;\"><span style=\"margin-right: 0.1em\">H</span><span style=\"vertical-align: super\">3</span><span>HO</span></span>",
