914a69e0c5bbb1744dc188a0093658e0b16a1867
fgamester <falvarez.munoz@hotmail.es>
2025-05-14 00:08:01 -0400

    Y^{99+}

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,13 @@
 <template>
   <span style="font-family: 'Times New Roman', Times, serif; display: inline-block; font-size: 16px;">
-    <span>[</span>
-    <span style="font-style: italic;">AgCl</span>
-    <span style="vertical-align: sub; font-size: 0.625em;">2</span>
-    <span>]</span>
-    <span style="vertical-align: 0.5em; line-height: 0.5em;">–</span>
+    <span style="font-style: italic; margin-right: 0.15em;">Y</span>
+    <span style="vertical-align: super; font-size: 0.625em;">99+</span>
   </span>
 </template>
 <script lang="ts" setup>
 const completed = {
   "": "",
+  "Y^{99+}": "<span style=\"font-family: 'Times New Roman', Times, serif; display: inline-block; font-size: 16px;\"><span style=\"font-style: italic; margin-right: 0.15em;\">Y</span><span style=\"vertical-align: super; font-size: 0.625em;\">99+</span></span>",
   "[AgCl_{2}]^{-}": "<span style=\"font-family: 'Times New Roman', Times, serif; display: inline-block; font-size: 16px;\"><span>[</span><span style=\"font-style: italic;\">AgCl</span><span style=\"vertical-align: sub; font-size: 0.75em;\">2</span><span>]</span><span style=\"vertical-align: 0.5em; line-height: 0.5em;\">–</span></span>",
   "CrO_{4}^{2-}": "<span style=\"font-family: 'Times New Roman', Times, serif; display: inline-block; font-size: 16px;\"><span style=\"font-style: italic;\">CrO</span><span style=\"display: inline-flex; flex-direction: column; line-height: 0.75em; vertical-align: middle; margin-left: 0.05em;\"><span style=\"font-size: 0.75em;\">2–</span><span style=\"font-size: 0.75em;\">4</span></span></span>",
   "n H_{2}O": "<span style=\"font-family: 'Times New Roman', Times, serif; display: inline-block; font-size: 16px;\"><span style=\"font-style: italic;\">nH</span><span style=\"vertical-align: sub; font-size: 0.75em;\">2</span><span style=\"font-style: italic;\">O</span></span>",
