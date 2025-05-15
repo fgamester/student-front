6790d1cda5dfc4dcee6b0f0329bb3da0c2cf5a49
fgamester <falvarez.munoz@hotmail.es>
2025-05-15 19:16:15 -0400

    uncommited changes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,17 @@
 <template>
   <span style="font-family: 'Times New Roman', Times, serif; display: inline-block; font-size: 16px;">
-    <span style="display: inline-flex;flex-direction: column; font-size: 0.625em; line-height: 0.5em; align-items: center; vertical-align: 0.75em; margin-right: 0.25em;">
+    <span>▢ + ▢</span>
+    <span
+      style="display: inline-flex;flex-direction: column; font-size: 0.625em; line-height: 0.5em; align-items: center; vertical-align: 0.75em; margin-right: 0.75em; margin-left: 0.75em;">
       <span>▢</span>
-      <span style="margin-bottom: 0.25em;">—</span>
+      <span style="display: inline-flex; margin-top: 0.125em; margin-bottom: 0.25em; transform: translateX(0.125em);">
+        <span>—</span>
+        <span>—</span>
+        <span style="margin-left: -0.5em; transform: translateY(0.1em);">></span>
+      </span>
       <span>▢</span>
     </span>
-    <span>▢</span>
-    <span style="font-size: 0.625em; vertical-align: sub;">▢</span>
-    <span>▢</span>
+    <span>▢ + ▢</span>
   </span>
 </template>
 <script lang="ts" setup>
@@ -23,6 +27,8 @@ const completed = {
   "#0_{#0}#0_{#0}": "<span style=\"font-family: 'Times New Roman', Times, serif; display: inline-block; font-size: 16px;\"><span>▢</span><span style=\"font-size: 0.625em; vertical-align: sub;\">▢</span><span>▢</span><span style=\"font-size: 0.625em; vertical-align: sub;\">▢</span></span>",
   "#0 #0_{#0}#0": "<span style=\"font-family: 'Times New Roman', Times, serif; display: inline-block; font-size: 16px;\"><span>▢</span><span>▢</span><span style=\"font-size: 0.625em; vertical-align: sub;\">▢</span><span>▢</span></span>",
   "\\tfrac{#0}{#0} #0_{#0}#0": "<span style=\"font-family: 'Times New Roman', Times, serif; display: inline-block; font-size: 16px;\"><span style=\"display: inline-flex;flex-direction: column; font-size: 0.625em; line-height: 0.5em; align-items: center; vertical-align: 0.75em; margin-right: 0.25em;\"><span>▢</span><span style=\"margin-bottom: 0.25em;\">—</span><span>▢</span></span><span>▢</span><span style=\"font-size: 0.625em; vertical-align: sub;\">▢</span><span>▢</span></span>",
+  "#0 \\ + \\ #0 \\ \\xlongequal[#0]{#0} \\ #0 \\ + \\ #0": "<span style=\"font-family: 'Times New Roman', Times, serif; display: inline-block; font-size: 16px;\"><span>▢ + ▢</span><span style=\"display: inline-flex;flex-direction: column; font-size: 0.625em; line-height: 0.5em; align-items: center; vertical-align: 0.75em; margin-right: 0.25em;\"><span>▢</span><span style=\"display: inline-flex; flex-direction: column; rotate: 90deg; gap: 0.2em; margin: -0.25em 1.125em -0.25em;\"><span>ǁ</span><span>ǁ</span></span><span>▢</span></span><span>▢ + ▢</span></span>",
+  "#0 \\ + \\ #0 \\ \\longrightarrow[#0]{#0} \\ #0 \\ + \\ #0": "<span style=\"font-family: 'Times New Roman', Times, serif; display: inline-block; font-size: 16px;\"><span>▢ + ▢</span><span style=\"display: inline-flex;flex-direction: column; font-size: 0.625em; line-height: 0.5em; align-items: center; vertical-align: 0.75em; margin-right: 0.75em; margin-left: 0.75em;\"><span>▢</span><span style=\"display: inline-flex; margin-top: 0.125em; margin-bottom: 0.25em; transform: translateX(0.125em);\"><span>—</span><span>—</span><span style=\"margin-left: -0.5em; transform: translateY(0.1em);\">></span></span><span>▢</span></span><span>▢ + ▢</span></span>",
   "": "",
   "\\overset{+4}{Mn}": "<span style=\"font-family: 'Times New Roman', Times, serif; display: inline-block; font-size: 16px; vertical-align: middle;\"><span style=\"display: inline-flex; flex-direction: column; line-height: 0.75em; vertical-align: 0.75em;\"><span style=\"font-size: 0.625em; align-self: center;\">+4</span><span style=\"font-style: italic;\">Mn</span></span></span>",
   "MnO_{2}": "<span style=\"font-family: 'Times New Roman', Times, serif; display: inline-block; font-size: 16px;\"><span style=\"font-style: italic;\">MnO</span><span style=\"font-size: 0.625em; vertical-align: sub;\">2</span></span>",
